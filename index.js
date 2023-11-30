cda87823bfbe3038c1def39c41e2f17a63ee7927
AdlinMH <adlinmuhammad18@gmail.com>
2023-11-30 21:46:03 +0700

    feat: add package - rneuifor ui components

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+
+const IGNORED_LOGS = [
+  'The native module for Flipper seems unavailable. Please verify that',
+]
+
+LogBox.ignoreLogs(IGNORED_LOGS)
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { PersistGate } from 'redux-persist/lib/integration/react';
 import { store, persistor } from './store';
 import ApplicationNavigator from './navigators/Application';
 import './translations';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   // setup sentry and reactotron config
@@ -19,16 +20,18 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      {/**
-       * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
-       * and saved to redux.
-       * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
-       * for example `loading={<SplashScreen />}`.
-       * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
-       */}
-      <PersistGate loading={null} persistor={persistor}>
-        <ApplicationNavigator />
-      </PersistGate>
+      <SafeAreaProvider>
+        {/**
+         * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
+         * and saved to redux.
+         * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
+         * for example `loading={<SplashScreen />}`.
+         * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
+         */}
+        <PersistGate loading={null} persistor={persistor}>
+          <ApplicationNavigator />
+        </PersistGate>
+      </SafeAreaProvider>
     </Provider>
   )
 }
