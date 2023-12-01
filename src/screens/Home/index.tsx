fe5c248b2e50888707382e0257ce0ad40b6525c9
AdlinMH <adlinmuhammad18@gmail.com>
2023-12-01 11:47:15 +0700

    feat: add rn-fast-image package

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
-import React, { useCallback, useEffect } from 'react'
-import { FlatList, Text, View } from 'react-native'
+import React, { useCallback } from 'react'
+import { FlatList, Text, View, ActivityIndicator } from 'react-native'
+import { responsiveWidth } from 'react-native-responsive-dimensions'
+import FastImage from 'react-native-fast-image'
 import { Card } from '@rneui/themed'
 
 import { useTheme } from '@/hooks'
-import { useGetBusinessesSearchQuery } from '@/apis/businesses'
 import { BottomSheetCustom } from '@/components'
+import { useGetBusinessesSearchQuery } from '@/apis/businesses'
 import { BusinessesGetSearchRes_Business } from '@/apis/businesses/_types'
 
 const LazyMapViewComponent = React.lazy(() => import('@/components/MapCustom'))
@@ -12,42 +14,58 @@ const LazyMapViewComponent = React.lazy(() => import('@/components/MapCustom'))
 function Home() {
   const { Layout, Fonts, Gutters } = useTheme()
 
-  const { data, error } = useGetBusinessesSearchQuery({ location: 'NYC' })
+  const { isLoading, data, error } = useGetBusinessesSearchQuery({ location: 'NYC' })
 
   const { status: errStatus, data: errData } = (error || {}) as any
 
   const renderItem = useCallback(({ item }: { item: BusinessesGetSearchRes_Business }) => (
-    <Card>
-      <Card.Title>{item.name}</Card.Title>
-      <Card.Divider />
-      <View>
-        {/* <Image
-          style={styles.image}
-          resizeMode="cover"
-          source={{ uri: u.avatar }}
-        /> */}
-        <Text>
-          {item.name}
-        </Text>
+    <Card containerStyle={[Gutters.radiusTiny, Gutters.marginHorizontalTiny, { width: responsiveWidth(80) }]}>
+      <View style={[Layout.row]}>
+        <FastImage
+          style={[
+            Gutters.radiusTiny, Gutters.marginRightTiny,
+            { width: responsiveWidth(30), height: responsiveWidth(30)}
+          ]}
+          source={{
+              uri: item.image_url,
+              priority: FastImage.priority.high,
+          }}
+          resizeMode={FastImage.resizeMode.cover}
+        />
+        <View >
+          <Card.Title>{item.name}</Card.Title>
+          <Card.Divider />
+          <View>
+            <Text>
+              {item.name}
+            </Text>
+          </View>
+        </View>
       </View>
     </Card>
   ), []);
 
-  useEffect(() => {
-    console.tron('data', data)
-  }, [data])
-
-  useEffect(() => {
-    console.tron('error', error)
-  }, [error])
-
   return (
     <View style={[Layout.fill, { backgroundColor: 'red' }]}>
+      {/* Map */}
       <LazyMapViewComponent />
 
+      {/* Display Loading */}
+      {isLoading && (
+        <View style={[Layout.fullHeight, Layout.fullWidth, Layout.positionAbsolute, Layout.center, Layout.backgroundWhite, Layout.opacity5, { zIndex: 10 }]}>
+          <ActivityIndicator size="large"/>
+        </View>
+      )}
+
+      {/* Display List */}
       <BottomSheetCustom isVisible={true} withBackdrop={false} panelStyle={[Layout.backgroundTransparent]}>
         {data && (
-          <FlatList horizontal data={data.businesses} renderItem={renderItem} showsHorizontalScrollIndicator={false} />
+          <FlatList
+            horizontal
+            data={data.businesses}
+            renderItem={renderItem}
+            showsHorizontalScrollIndicator={false}
+          />
         )}
       </BottomSheetCustom>
 
